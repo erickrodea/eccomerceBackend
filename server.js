6736d01de02340f8b8d6dcccdc8b01ce5634c29a
Erick Rodea <rodeaerick@yahoo.com>
2024-01-20 17:45:38 -0500

    cwcwvwd

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get("/", (req, res) => {
+app.get("/products", (req, res) => {
   return res.json("from backend side");
 });
 
